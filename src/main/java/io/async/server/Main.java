37328e15ae169201ce7f826884702595d47200c8
Nabeel Ali Memon <nimtiazm@gmail.com>
2017-09-29 03:26:56 +0400

    Initial commit.

---
+++ src/main/java/io/async/server/Main.java
@@ -0,0 +1,9 @@
+package io.async.server;
+
+public class Main
+{
+	public static void main(String[] args)
+	{
+		System.out.println("hello java9");
+	}
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,4 @@
+module server {
+  requires java.base;
+  exports io.async.server;
+}
